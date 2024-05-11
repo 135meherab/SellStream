71156fb94c3a8b30cf97253a2459c5dc734ab624
almusfuad <almusfuad@gmail.com>
2024-05-12 02:44:29 +0600

    [resolve] products data

--- product/serializers.py
+++ product/serializers.py
@@ -1,23 +1,19 @@
 from rest_framework import serializers
-<<<<<<< HEAD
-=======
 from django.utils.text import slugify 
->>>>>>> c1d3c7f76dd2f2a4d6b3589fccf6c8b88f483f9a
 from .models import Category,Customer,Uom,Product, Order
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-<<<<<<< HEAD
         fields = '__all__'
-=======
+
         fields = ['id','name']
     def to_representation(self, instance):
         data = super().to_representation(instance)
         if 'name' in data and 'slug' not in data:
             data['slug'] = slugify(data['name'])
         return data
->>>>>>> c1d3c7f76dd2f2a4d6b3589fccf6c8b88f483f9a
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -37,9 +33,7 @@ class ProductSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-<<<<<<< HEAD
         fields = '__all__'
-=======
         fields = ['id','product_order','customer','quantity']
 
     def create(self, validated_data):
@@ -52,7 +46,6 @@ class OrderSerializer(serializers.ModelSerializer):
         total = quantity * price
         validated_data['Total'] = total
         return super().create(validated_data)
->>>>>>> c1d3c7f76dd2f2a4d6b3589fccf6c8b88f483f9a
 
 # class OrderDetailsSerializer(serializers.ModelSerializer):
 #     class Meta:
