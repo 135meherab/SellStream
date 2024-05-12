81ee3f638b3372484036897e6c7055ed87bfb64d
meherab <hunterkali66@gmail.com>
2024-05-12 13:02:45 +0600

    [Updated] returned to sql

--- sellstream/settings.py
+++ sellstream/settings.py
@@ -96,12 +96,12 @@ WSGI_APPLICATION = 'sellstream.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -113,13 +113,13 @@ WSGI_APPLICATION = 'sellstream.wsgi.app'
 #         "PORT": "5432"
 #     }
 # }
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://postgres:postgres@localhost:5432/mysite',
+#         conn_max_age=600
+#     )
+# }
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
