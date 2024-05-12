30f2302f48f3ee5e6b31bd8899127f3b60445d54
meherab <hunterkali66@gmail.com>
2024-05-12 09:27:23 +0600

    [Changed] db

--- sellstream/settings.py
+++ sellstream/settings.py
@@ -85,16 +85,27 @@ TEMPLATES = [
 
 
 
-WSGI_APPLICATION = 'sellstream.wsgi.application'
+WSGI_APPLICATION = 'sellstream.wsgi.app'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DATABASE"),
+        "USER": os.getenv('POSTGRES_USER'),
+        "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": "5432"
     }
 }
 
@@ -146,3 +157,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
