02d51c6e4943b130e50ad131abd33cc7751e722e
H-M-Nizum <hmnizum1714032@gmail.com>
2024-05-15 00:59:54 +0600

    [Update] : Employee management system

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import DesignationModel, EmployeeModel, AttendanceModel, LeaveModel
 # Register your models here.
+
+
+admin.site.register(DesignationModel)
+
+admin.site.register(EmployeeModel)
+
+admin.site.register(AttendanceModel)
+
+admin.site.register(LeaveModel)
