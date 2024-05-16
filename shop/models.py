cfb4b3c547db837c736e828eca5d1b9e12d0140b
almusfuad <almusfuad@gmail.com>
2024-05-16 19:17:33 +0600

    [change] ordering or cors-headersin settings

--- shop/models.py
+++ shop/models.py
@@ -8,8 +8,16 @@ class Shop(models.Model):
       address = models.TextField()
       phone = models.CharField(max_length=20)
       
+      
+      def __str__(self):
+            return self.name
+      
 
 class Branch(models.Model):
       shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
       name = models.CharField(max_length=50)
       location = models.TextField()
+      
+      
+      def __str__(self):
+            return self.name
