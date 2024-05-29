46d9e1e47e96f75ea31378a2ecd51955f050f000
Shifat-project <shifat32153@gmail.com>
2024-05-29 10:51:56 +0600

    [add] user_datails isadmin json

--- employee/views.py
+++ employee/views.py
@@ -18,7 +18,7 @@ class DesignationViews(viewsets.ModelViewSet):
     serializer_class = DesignationSerializers
 
     permission_classes = [IsAuthenticated]
-    # authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication]
 
     def get_queryset(self):
         try:
@@ -36,7 +36,7 @@ class EmployeeViews(viewsets.ModelViewSet):
     queryset = EmployeeModel.objects.all()
     serializer_class = EmployeeSerializers
 
-    # authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
 
